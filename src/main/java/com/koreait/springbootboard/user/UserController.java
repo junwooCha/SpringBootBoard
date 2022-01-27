631f3222fcf1db894a70ea75e98931bc4d5bf562
Junwoo <chaw510@gmail.com>
2022-01-27 16:49:54 +0900

    Initial commit

---
+++ src/main/java/com/koreait/springbootboard/user/UserController.java
@@ -0,0 +1,15 @@
+package com.koreait.springbootboard.user;
+
+import com.koreait.springbootboard.user.model.UserEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/login")
+    public void login(@ModelAttribute UserEntity userEntity){}
+}
